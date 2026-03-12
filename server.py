dda5ea613f815129f6f645a69eb77b063607f914
saif2000-ee <siefinfodz@gmail.com>
2026-03-12 21:50:02 +0100

    Modify server execution to use environment variable for port
    Updated the main execution block to use an environment variable for the port.

--- server.py
+++ server.py
@@ -66,5 +66,6 @@ def predict():
 
     return jsonify({'prediction':'?'})
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
