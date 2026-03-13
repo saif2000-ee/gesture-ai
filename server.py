c630e3f085930147d697364dae8afabf69c8f3bb
saif2000-ee <siefinfodz@gmail.com>
2026-03-13 02:47:20 +0100

    Fix string quotes in main check

--- server.py
+++ server.py
@@ -64,6 +64,6 @@ def predict():
     return jsonify({'prediction': '?'})
 import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
