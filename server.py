0a7955044cf9842243106c16e2ef6dadde7e89dd
saif2000-ee <siefinfodz@gmail.com>
2026-03-13 02:40:29 +0100

    Update server.py

--- server.py
+++ server.py
@@ -63,6 +63,6 @@ def predict():
 
     return jsonify({'prediction': '?'})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+if __name__=='__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
